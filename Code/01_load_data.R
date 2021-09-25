aa29ed1f63204b3c6b19e162cf057bca88153acf
azinkan-01 <91105231+azinkan-01@users.noreply.github.com>
2021-09-25 14:51:55 -0700

    "Restructured files for path, and completed first pass of the Week 1 project"

---
+++ Code/01_load_data.R
@@ -0,0 +1,73 @@
+# Import packages
+library(tidyverse)
+library(haven)
+library(magrittr)
+library(dplyr)
+library(ggplot2)
+
+# Load Raw data as Data Frame
+nfhs <- read_dta("Raw_Data/IAHR52FL.dta")
+
+# Print a sample of what the data looks like
+head(nfhs)
+
+# Number of unique households in the data set
+print(count(nfhs['hv002']))
+
+# How many variables are there
+print(length(colnames(nfhs)))
+
+
+# Sub setting me columns hhid : shstruc
+nfhs_cut_col = subset(nfhs, select = c(hhid : shstruc))
+# Print number of columns remaining
+print(length(colnames(nfhs_cut_col)))
+
+
+#Plot histogram as save the image to figures folder
+
+png("Figures/rplot.pdf") 
+
+ggplot(data= nfhs_cut_col,
+       mapping = aes(x= hv009), binwidth = 1) +
+       geom_histogram() +
+       xlab("Number of house hold members")
+dev.off()
+
+
+
+#Create a new data frame that contains only urban households (the variable is "hv025").
+# rural == 2
+new_df <- nfhs %>%
+  filter(hv025 == 2)
+
+# Summarize the records count by the rural group (Should only see one group)
+new_df %>%
+  group_by(hv025) %>%
+  summarise(
+    count = n()
+  )
+  
+
+# box plot of distribution of household size by type of urban area
+png("Figures/rplot_hist.pdf") 
+
+urban_df <- filter(nfhs, hv025 == 1)
+p <- ggplot(urban, mapping = aes(x= hv026 , y= hv009, group =1)) + 
+  geom_boxplot()
+p
+
+dev.off()
+
+
+# Use "group_by" and "summaries" to find the means and medians of the number of 
+#household members by type of urban area. Match the correct means to their urban areas.
+
+
+urban_df %>%
+  group_by(hv026) %>%
+  summarise(
+    mean(hv009), 
+    median(hv009)
+  )
+
